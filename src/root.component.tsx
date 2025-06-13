bb5138c1c6453f63a00b90c0fb463f4187af6c4c
Mike Will <Mike.Rego@baymetrics.com.br>
2025-06-13 14:56:38 -0300

    feat: validate if user is verified

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,17 +1,26 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignUp from "./auth/front-end/pages/sign-up";
 import SignIn from "./auth/front-end/pages/sign-in";
+import { isAuthenticated } from "./auth/back-end/services/is-authenticated";
+import Home from "./auth/front-end/pages/home";
 
 export default function Root(props) {
   return (
-    <div>
-      <h1>teste</h1>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={isAuthenticated("1") ? <Home /> : <SignIn />}
+        />
+        <Route
+          path="/signin"
+          element={isAuthenticated("1") ? <Home /> : <SignIn />}
+        />
+        <Route
+          path="/signup"
+          element={isAuthenticated("1") ? <Home /> : <SignUp />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
