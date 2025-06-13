5523c5b7cd13cf6a2694f15148f4ec7faf0aedc6
Mike Will <Mike.Rego@baymetrics.com.br>
2025-06-13 18:34:51 -0300

    chore: update mock authentication data

--- src/auth/back-end/mock-users.ts
+++ src/auth/back-end/mock-users.ts
@@ -5,7 +5,25 @@ export const mockUsers = [
     id: "1",
     email: "mikewillbento@gmail.com",
     passwordHash: sha512("Teste1234!@#").toString(),
-    isAuthenticated: false,
+    isAuthenticated: true,
     accessToken: "12345",
+  },
+];
+
+export const mockBuildings = [
+  {
+    id: "1",
+    foto: "FotoPredio1",
+    nome: "Baymetrics",
+  },
+  {
+    id: "2",
+    foto: "FotoPredio2",
+    nome: "The members",
+  },
+  {
+    id: "3",
+    foto: "FotoPredio3",
+    nome: "Empire State",
   },
 ];
